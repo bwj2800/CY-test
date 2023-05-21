912332c4ca9a02e64cd5d9d4f13ccc27452b57de
bwj2800 <bwj2800@naver.com>
2023-05-21 20:15:26 +0900

    FEAT material-ui to mui

--- client/src/components/CompanyTable.js
+++ client/src/components/CompanyTable.js
@@ -1,44 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Delete from "@material-ui/icons/Delete";
-import Edit from "@material-ui/icons/Edit";
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TablePagination from '@mui/material/TablePagination';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
+const columns = [
+  { id: 'id', label: 'id', minWidth: 50 },
+  { id: 'name', label: '기업명', minWidth: 100 },
+  { id: 'website',label: '홈페이지', minWidth: 170 },
+  { id: 'contact', label: '연락처', minWidth: 170 },
+  { id: 'owner', label: '대표', minWidth: 100 },
+  { id: 'edit', label: '수정', minWidth: 50 },
+  { id: 'delete', label: '삭제', minWidth: 50 },
+];
 
 export default function CustomizedTables() {
   
-  const classes = useStyles();
   const [rows, setRows] = useState([]);
 
   useEffect(() => {
@@ -46,37 +37,98 @@ export default function CustomizedTables() {
       .then(res => setRows(res.data))
       .catch(err => console.log(err))
   });
+  
+  const [open, setOpen] = React.useState(false);
+  const [cur_id, setCurId] = React.useState(-1);
+
+  const handleClickOpen = (id) => {
+    setCurId(id);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   return (
-    <TableContainer component={Paper} sx={{ maxHeight: 440, overflow: 'scroll'}}>
-      <Table stickyHeader className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>ID</StyledTableCell>
-            <StyledTableCell>기업명</StyledTableCell>
-            <StyledTableCell>홈페이지</StyledTableCell>
-            <StyledTableCell>연락처</StyledTableCell>
-            <StyledTableCell>대표</StyledTableCell>
-            <StyledTableCell>수정</StyledTableCell>
-            <StyledTableCell>삭제</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.id}>
-              <StyledTableCell component="th" scope="row">
-                {row.id}
-              </StyledTableCell>
-              <StyledTableCell>{row.name}</StyledTableCell>
-              <StyledTableCell>{row.website}</StyledTableCell>
-              <StyledTableCell>{row.contact}</StyledTableCell>
-              <StyledTableCell>{row.owner}</StyledTableCell>
-              <StyledTableCell><Edit/></StyledTableCell>
-              <StyledTableCell><Delete/></StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 420 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.id ==='edit'
+                            ? <EditIcon/>
+                            : column.id ==='delete'
+                              ? <DeleteIcon onClick={()=>handleClickOpen(row['id'])}/>
+                              : value}
+                        </TableCell>
+                      );
+                    })}
+                    
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+      <Dialog open={open} onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">
+          {"삭제 확인"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            id {cur_id}의 데이터를 정말로 삭제하시겠습니까?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>취소</Button>
+          <Button onClick={handleClose} autoFocus>삭제</Button>
+        </DialogActions>
+      </Dialog>
+    </Paper>
   );
 }
